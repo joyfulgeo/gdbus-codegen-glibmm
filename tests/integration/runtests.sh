c2ee74590fcbef6043260a8b2eac8a4b03adab45
Jioh Yoo <joyful.jioh.yoo@gmail.com>
2021-05-03 16:35:47 +0900

    add Test case transfering null embedding byte array

--- tests/integration/runtests.sh
+++ tests/integration/runtests.sh
@@ -2,15 +2,18 @@
 
 DIR="$(cd $(dirname "$0"); pwd)/../.."
 
+echo
+echo "== Build Stub"
+echo
 cd stub
 mkdir -p build
 cd build
 cd .. ; rm -rf build ; mkdir build; cd build; cmake .. -DCODEGEN="$DIR/gdbus-codegen-glibmm.py" -DCMAKE_BUILD_TYPE=Debug
 make
-./stubtest &
-STUB_PID=$!
 
-trap 'kill $STUB_PID' EXIT
+echo
+echo "== Build Proxy"
+echo
 
 cd ../../proxy
 mkdir -p build
@@ -18,4 +21,22 @@ cd build
 cd .. ; rm -rf build ; mkdir build; cd build; cmake .. -DCODEGEN="$DIR/gdbus-codegen-glibmm.py" -DCMAKE_BUILD_TYPE=Debug
 
 make
-./proxytest
+
+cd ../../
+
+echo
+echo "== Run Stub"
+echo
+
+./stub/build/stubtest &
+STUB_PID=$!
+
+trap 'kill $STUB_PID' EXIT
+
+sleep 3
+
+echo
+echo "== Run Proxy"
+echo
+
+./proxy/build/proxytest
